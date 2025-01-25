76f248577ad0d1f6157eb99727a7b10c75a88825
Sachith-SL <itsmesachith@gmail.com>
2025-01-25 09:28:34 +0530

    add two microservices

---
+++ employee/employee/src/main/java/com/sachith/employee/model/Employee.java
@@ -0,0 +1,56 @@
+package com.sachith.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private String phoneNumber;
+    private String address;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, String phoneNumber, String address) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
